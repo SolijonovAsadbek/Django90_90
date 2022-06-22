0431cc01231a87d253e4a3bdb6bc80597d7e6a0e
Asadbek <solijonovasadbek25111999@gmail.com>
2022-06-22 12:12:26 +0500

    commit

--- first_webPage/views.py
+++ first_webPage/views.py
@@ -2,16 +2,17 @@
 from django.http import HttpResponse
 from django.utils import timezone
 from random import randint
-
+from articles.models import Article
 
 def home(request):
     name = 'Django'
     date = timezone.now()
-    number = randint(1, 100)
+    number = randint(1, 3)
     # from database
-    # title =
-    # content =
-    H1_STRING = f"""<h1>Hello {name}-Time: {date}</h1>"""
-    P_STRING = f"""<p>Hello {number}</p>"""
+    obj = Article.objects.get(id=number)
+    title = obj.title
+    content = obj.content
+    H1_STRING = f"""<h1>{title} (id: {obj.id})</h1>"""
+    P_STRING = f"""<p>{content}</p>"""
     HTML_STRING = H1_STRING + P_STRING
     return HttpResponse(HTML_STRING)
