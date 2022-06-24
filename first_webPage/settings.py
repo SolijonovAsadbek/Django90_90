b795de03ea0c43be9968717c7399d97484167350
Asadbek <solijonovasadbek25111999@gmail.com>
2022-06-24 10:12:30 +0500

    templates

--- first_webPage/settings.py
+++ first_webPage/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'first_webPage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],   # O'zgaruvchan link yozish.
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- first_webPage/views.py
+++ first_webPage/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.utils import timezone
 from random import randint
 from articles.models import Article
+from django.template.loader import render_to_string
 
 
 def home(request):
@@ -11,12 +12,12 @@ def home(request):
     number = randint(1, 3)
     # from database
     obj = Article.objects.get(id=number)
-    
-    context = {'title ': obj.title,
+
+    # context
+    context = {'title': obj.title,
                'id': obj.id,
                'content': obj.content}
     # Django templates
-    HTML_STRING = """<h1>{title} (id: {id})</h1>
-                <p>{content}</p>""".format(**context)
+    HTML_STRING = render_to_string(template_name='home.html', context=context)
 
     return HttpResponse(HTML_STRING)
