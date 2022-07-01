4fe235fc4320138baf70ba25382ba133a2a6bc7e
Asadbek <solijonovasadbek25111999@gmail.com>
2022-07-01 11:35:37 +0500

    login

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(f'username: {username}\npassword: {password}')
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is None:
+            context = {'error': 'You don`t have a register.'}
+            return render(request, template_name='accounts/login.html', context=context)
+        login(request, user)
+        return redirect(to='/admin')
+
     return render(request=request, template_name='accounts/login.html')
+
 
 def logout_view(request):
     return render(request=request, template_name='accounts/logout.html')
+
 
 def register_view(request):
     return render(request=request, template_name='accounts/register.html')
