b5075dc52aa8714df5d005ea86fe40f92614c997
Asadbek <solijonovasadbek25111999@gmail.com>
2022-07-13 02:28:35 +0500

    Charfiled

--- articles/models.py
+++ articles/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=200)
     content = models.TextField()
