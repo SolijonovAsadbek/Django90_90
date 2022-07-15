7ac5e3c3327908f7e341adec26f9e4385907bb8a
Asadbek <solijonovasadbek25111999@gmail.com>
2022-07-15 08:48:19 +0500

    relationships

--- test_model_layer/admin.py
+++ test_model_layer/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from test_model_layer.models import Runner
+from test_model_layer.models import Runner, Person, Group, Membership, Publication, Article, Place, Restaurant, Waiter
 
 
 class RunnerAdmin(admin.ModelAdmin):
     list_display = ['name', 'medal']
 
 admin.site.register(Runner, RunnerAdmin)
+admin.site.register(Person)
+admin.site.register(Group)
+admin.site.register(Membership)
+admin.site.register(Publication)
+admin.site.register(Article)
+admin.site.register(Place)
+admin.site.register(Restaurant)
+admin.site.register(Waiter)
--- test_model_layer/models.py
+++ test_model_layer/models.py
@@ -9,3 +9,76 @@ class Runner(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(Person, through='Membership')
+
+    def __str__(self):
+        return f'{self.name} {self.members.count()} {self.members.all()}'
+
+
+class Membership(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    date_joined = models.DateField()
+    invite_reason = models.CharField(max_length=64)
+
+    def __str__(self):
+        return "{} is in {}".format(self.person.name, self.group.name)
+
+
+class Publication(models.Model):
+    title = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return f"{self.title} {self.id}"
+
+class Article(models.Model):
+    headline = models.CharField(max_length=100)
+    publications = models.ManyToManyField(Publication)
+
+    class Meta:
+        ordering = ['headline']
+
+    def __str__(self):
+        return f'{self.headline} {self.publications.count()} {self.publications.all()}'
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=80)
+
+    def __str__(self):
+        return "%s the place" % self.name
+
+class Restaurant(models.Model):
+    place = models.OneToOneField(
+        Place,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    serves_hot_dogs = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "%s the restaurant" % self.place.name
+
+class Waiter(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "%s the waiter at %s" % (self.name, self.restaurant)
